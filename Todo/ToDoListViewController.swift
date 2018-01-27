367d8b536880e3a0dc2b3f93fc820e0e41e023c1
patrick <leepc777@gmail.com>
2018-01-27 15:28:23 -0800

    use userDefaults method to persist Data

--- Todo/ToDoListViewController.swift
+++ Todo/ToDoListViewController.swift
@@ -12,18 +12,16 @@ class ToDoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    
+//        itemArray = defaults.array(forKey: "ToDoListArray") as! [String]
+        if let itemArray = defaults.array(forKey: "ToDoListArray") as? [String] {
+        self.itemArray = itemArray
+        }
     }
 
-    var itemArray = ["Find Mike","Buy Eggos","Destory Demogorgon"]
+    var itemArray = ["Find Mike","Buy Eggos","Destory fire Demogorgon"]
 
-
+    let defaults = UserDefaults.standard
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -63,8 +61,8 @@ class ToDoListViewController: UITableViewController {
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
-            print(alertTextField.text)
-            print("trigger alert.addTextField")
+//            print(alertTextField.text)
+//            print("trigger alert.addTextField")
             texField = alertTextField // pass local textField to outside this closure
         }
         
@@ -82,7 +80,8 @@ class ToDoListViewController: UITableViewController {
          */
             self.itemArray.append(texField.text!)
 //            self.itemArray.append(texField.text ?? "Default Value")
-            print(self.itemArray)
+            self.defaults.setValue(self.itemArray, forKey: "ToDoListArray")
+//            print(self.itemArray)
             self.tableView.reloadData()
         }
 
@@ -90,51 +89,5 @@ class ToDoListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
- 
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
